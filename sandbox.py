4db406750ef419b2f72ffbeb517d16d1c4e0718f
jduust <jakobduus@gmail.com>
2026-02-11 11:57:11 +0100

    updated sandbox for queue handling

--- sandbox.py
+++ sandbox.py
@@ -3,6 +3,12 @@
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
 
 from robot_framework.process import process
+from robot_framework import config
+from robot_framework import reset
+from OpenOrchestrator.database.queues import QueueStatus
+from robot_framework.exceptions import handle_error, BusinessError
+
+
 import os
 
 # pylint: disable-next=unused-argum
@@ -13,4 +19,12 @@ orchestrator_connection = OrchestratorConnection(
     None,
 )
 
-process(orchestrator_connection)
+reset.reset(orchestrator_connection)
+
+queue_element = orchestrator_connection.get_next_queue_element(config.QUEUE_NAME)
+
+try:
+    process(orchestrator_connection, queue_element)
+    orchestrator_connection.set_queue_element_status(queue_element.id, QueueStatus.DONE)
+except BusinessError as error:
+    handle_error("Business Error", error, queue_element, orchestrator_connection)
